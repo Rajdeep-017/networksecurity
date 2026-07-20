1488dfaf5cadd4b98bd284fe700967d4fbfc63dd
Rajdeep <rajdeepsawant919@gmail.com>
2026-07-20 19:46:51 +0530

    pipeline complete

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -10,6 +10,7 @@ class trainingpipelineconfig:
         self.pipeline_name=train_pipeline.PIPELINE_NAME
         self.artifact_name=train_pipeline.ARTIFACT_DIR
         self.artifact_dir=os.path.join(self.artifact_name,timestamp)
+        self.model_dir=os.path.join("final_model")
         self.timestamp:str=timestamp
         
 
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -6,6 +6,8 @@ from networksecurity.exception.exception import Networksecurity
 from networksecurity.utils.ml_utils.model.estimator import Networkmodel
 from networksecurity.logging.logger import logging
 import os,sys
+from networksecurity.cloud.s3_syncer import S3Sync
+from networksecurity.constant.train_pipeline import TRAINING_BUCKET_NAME,SAVED_MODEL_DIR
 
 from networksecurity.entity.config_entity import (
     trainingpipelineconfig,
@@ -23,6 +25,7 @@ from networksecurity.entity.artifact_entity import(
 class Trainingpipeline:
     def __init__(self):
         self.training_pipeline_config=trainingpipelineconfig()
+        self.s3_sync=S3Sync()
     
     def start_data_ingestion(self):
         try:
@@ -68,12 +71,31 @@ class Trainingpipeline:
         except Exception as e:
             raise Networksecurity(e,sys)
     
+    #local artifact is going to  s3 bucket
+    def sync_arifact_dir_to_s3(self):
+        try:
+            aws_bucket_url=f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise Exception(e,sys)
+    #local final model is going to S3 bucket
+
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url=f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise Networksecurity(e,sys)
+
+    
     def run_pipeline(self):
         try:
             data_ingestion_artifact=self.start_data_ingestion()
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            self.sync_arifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
             return model_trainer_artifact
         except Exception as e:
             raise Networksecurity(e,sys)
